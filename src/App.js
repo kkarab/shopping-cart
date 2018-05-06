0489238f08d0779edf25006aaf8e77858347b1d6
kkarab <kostiskarampelas@yahoo.com>
2018-05-06 23:40:44 +0200

    step 9 completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CartItem from './components/CartItem.js' ;
+import Clock from 'react-live-clock';
 
 
 const products = [
@@ -33,6 +34,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <Clock format={'HH:mm:ss'} ticking={true} />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
